a84bce98d80786c270a5c24978c77643e97431ab
João Augusto <joao.augusto@inevent.com>
2025-02-08 17:05:32 -0300

    chore: install laravel sanctum

--- api-friend-challenge/app/Providers/AppServiceProvider.php
+++ api-friend-challenge/app/Providers/AppServiceProvider.php
@@ -2,23 +2,32 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\PersonalAccessToken;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+    }
+
+    /**
+     * Apply commands
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands($this->app->environment('production'));
+        Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
     }
 }

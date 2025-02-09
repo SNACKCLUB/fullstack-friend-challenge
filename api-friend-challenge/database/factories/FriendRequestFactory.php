7654d82ac81501217589c82bec186c4445236771
João Augusto <joao.augusto@inevent.com>
2025-02-09 07:42:57 -0300

    env: add some seeders

--- api-friend-challenge/database/factories/FriendRequestFactory.php
+++ api-friend-challenge/database/factories/FriendRequestFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,6 +17,8 @@ class FriendRequestFactory extends Factory
      */
     public function definition(): array
     {
-        return [];
+        return [
+            'requested_user_id' => User::factory()->create()->id,
+        ];
     }
 }
--- api-friend-challenge/database/seeders/FriendRequestSeeder.php
+++ api-friend-challenge/database/seeders/FriendRequestSeeder.php
@@ -2,9 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\FriendRequest;
 use Illuminate\Database\Seeder;
 
 class FriendRequestSeeder extends Seeder
 {
-    public function run(): void {}
+    public function run(): void
+    {
+        // Create 5 friend requests for the first user
+        FriendRequest::factory(5)->create([
+            'user_id' => 1
+        ]);
+    }
 }

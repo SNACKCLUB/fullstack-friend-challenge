3c50349fcccc3d00044b578f6f2ef24f8aa36188
João Augusto <joao.augusto@inevent.com>
2025-02-10 09:55:46 -0300

    improve: login form validations

---
+++ friend-challenge/src/components/Utils/input-group.tsx
@@ -0,0 +1,42 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+type InputGroupProps = {
+  children: ReactNode;
+};
+
+type InputGroupLabelProps = {
+  children: ReactNode;
+  htmlFor: string;
+};
+
+type InputGroupErrorLabelProps = {
+  message?: string;
+};
+
+function InputGroup({ children }: InputGroupProps) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col gap-2",
+        "[&_input]:h-[40px] [&_input]:px-4 [&_input]:rounded-md",
+        "[&_input]:bg-gray-950/50 [&_input]:outline-none [&_input]:focus:outline-red-400"
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+function Label({ children, htmlFor }: InputGroupLabelProps) {
+  return <label htmlFor={htmlFor}>{children}</label>;
+}
+
+function ErrorLabel({ message }: InputGroupErrorLabelProps) {
+  return <span className="text-red-400 text-sm">{message}</span>;
+}
+
+InputGroup.Label = Label;
+InputGroup.ErrorLabel = ErrorLabel;
+
+export default InputGroup;
--- friend-challenge/src/components/forms/loginForm.tsx
+++ friend-challenge/src/components/forms/loginForm.tsx
@@ -1,32 +1,62 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import InputGroup from "../Utils/input-group";
+
+const loginFormSchema = z.object({
+  email: z.string().nonempty("Email is required!").email("Email is invalid!"),
+  password: z
+    .string()
+    .nonempty("Password is required!")
+    .min(8, "password must have at least 8 characters"),
+});
+
+type LoginFormData = z.infer<typeof loginFormSchema>;
+
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(loginFormSchema),
+  });
+
+  const onSubmit = (data: LoginFormData) => {
+    console.log("xxxx", data);
+  };
+
   return (
-    <form action="" className="flex flex-col gap-4 w-[80%] text-sm">
-      <div className="flex flex-col gap-2">
-        <label htmlFor="email" className="">
-          Email
-        </label>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-4 w-[80%] text-sm"
+    >
+      <InputGroup>
+        <InputGroup.Label htmlFor="email">Email</InputGroup.Label>
         <input
           type="email"
-          className="h-[40px] px-4 rounded-md bg-gray-950/50 outline-none focus:outline-red-400"
-          name="email"
-          id="email"
           placeholder="Your email"
           required
+          {...register("email")}
         />
-      </div>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="password" className="">
-          Password
-        </label>
+        {errors.email && (
+          <InputGroup.ErrorLabel message={errors.email.message} />
+        )}
+      </InputGroup>
+      <InputGroup>
+        <InputGroup.Label htmlFor="password">Password</InputGroup.Label>
         <input
           type="password"
-          className="h-[40px] px-4 rounded-md bg-gray-950/50 outline-none focus:outline-red-400"
-          name="password"
-          id="password"
           placeholder="Your password"
           required
+          {...register("password")}
         />
-      </div>
+        {errors.password && (
+          <InputGroup.ErrorLabel message={errors.password.message} />
+        )}
+      </InputGroup>
       <div className="py-4">
         <button
           className="bg-gray-800 w-full h-[40px] rounded-md outline-none hover:outline-red-400"
